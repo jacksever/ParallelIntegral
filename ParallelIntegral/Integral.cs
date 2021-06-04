e5d9a1d3e7b6ed921262bafc38decd77a48ec0bb
Alexander Gorodnikov <aleks_gor2000@mail.ru>
2021-06-04 18:48:00 +0300

    init

---
+++ ParallelIntegral/Integral.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Threading;
+
+namespace ParallelIntegral
+{
+	public class Integral
+	{
+		private readonly Semaphore semaphore = new(1, 1);
+		private readonly Func<double, double> function;
+
+		private readonly double left;
+		private readonly double right;
+		private readonly double eps;
+		private double result = 0;
+
+		public Integral(Func<double, double> function, double left, double right, double eps)
+		{
+			this.function = function;
+			this.left = left;
+			this.right = right;
+			this.eps = eps;
+		}
+
+		public double GetResult => result;
+
+		public void Start()
+		{
+			new Thread(Calculate).Start(new double[] { left, right });
+		}
+
+		private void Calculate(object items)
+		{
+			semaphore.WaitOne();
+			var numbers = (double[])items;
+
+			var left = numbers[0];
+			var right = numbers[1];
+
+			var distance = right - left;
+			var height = distance / 2;
+			var center = left + height;
+
+			if (distance < eps)
+			{
+				result += function(center) * distance;
+			}
+			else
+			{
+				new Thread(Calculate).Start(new double[] { left, center });
+				new Thread(Calculate).Start(new double[] { center, right });
+			}
+
+			semaphore.Release();
+		}
+	}
+}
---
+++ ParallelIntegral/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Threading;
+
+namespace ParallelIntegral
+{
+	public class Program
+	{
+		public static void Main()
+		{
+			Console.WriteLine($"Интеграл x / Sqrt(x^4 + 16), промежуток от 0 до Sqrt(3)");
+
+			Integral integral = new(x => x / Math.Sqrt(Math.Pow(x, 4)+ 16), 0, Math.Sqrt(3), 0.001);
+			integral.Start();
+			Thread.Sleep(2000);
+			Console.WriteLine($"Интеграл равен: {integral.GetResult}");
+			Console.ReadKey();
+		}
+	}
+}
